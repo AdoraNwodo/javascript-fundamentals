2e2a5896573e14b38c0ec99fe567f52d5390cc80
nennenwodo <nennenwodo@gmail.com>
2018-11-12 12:38:42 +0100

    Fourth lesson, IIFEs

--- beginner/iifes.js
+++ beginner/iifes.js
@@ -4,4 +4,20 @@
     iifes.js
 
     IIFE - Immediately Invoked Function Expression
+
+    Running the function immediatey after its declared.
 */
+
+//This is an example of an IIFE. It is executed immediatey it is declared.
+(function(){
+    var status = "ready";
+    alert(status);
+})();
+
+//This is a regular function
+function init(){
+    var status = "ready";
+    alert(status);
+}
+//The regular function would not run unti it is called manually, like this.
+init();
