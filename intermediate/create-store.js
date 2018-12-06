5199cb50cdad01270f63bed939c1bc4558bbf6fe
nennenwodo <nennenwodo@gmail.com>
2018-12-06 19:12:59 +0100

    Added reducers

--- intermediate/create-store.js
+++ intermediate/create-store.js
@@ -1,6 +1,7 @@
-//Create a sample data store
+//LIBRARY CODE
 
-function createStore(){
+//Create a sample data store
+function createStore(reducer){
 
     /*
      * The store should have four parts
@@ -25,9 +26,74 @@ function createStore(){
         }
     }
 
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener() )
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 
 }
+
+
+//APP CODE
+function todos(state = [], action){
+
+    switch(action.type){
+        case 'ADD_TODO' :
+            return state.concat([action.todo]) 
+        case 'REMOVE_TODO' :
+            return state.filter((todo) => todo.id !== action.id)
+        case 'TOGGLE_TODO' :
+            return state.map((todo) => todo.id !== action.id ? todo:
+            Object.assign({}, todo, {complete: !todo.complete}))
+        default:
+            return state
+    }
+
+    /*
+    if(action.type === 'ADD_TODO'){
+        return state.concat([action.todo]) 
+    }else if(action.type === 'REMOVE_TODO'){
+        return state.filter((todo) => todo.id !== action.id)
+    }else if(action.type === 'TOGGLE_TODO'){
+        return state.map((todo) => todo.id !== action.id ? todo:
+            Object.assign({}, todo, {complete: !todo.complete})
+        )
+    }else{
+        return state
+    }
+    */
+}
+
+
+function goals(state = [], action){
+    switch(action.type){
+        case 'ADD_GOAL' :
+            return state.concat([action.goal]) 
+        case 'REMOVE_GOAL' :
+            return state.filter((goal) => goal.id !== action.id)
+        default:
+            return state
+    }
+}
+
+function app(){}
+
+const store = createStore(todos)
+store.subscribe(() => {
+    console.log('The new state is: ', store.getState())
+})
+
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false
+    }
+})
